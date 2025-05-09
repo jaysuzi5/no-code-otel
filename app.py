a94a0241ba26ea44a32cd2811ee41e7b16fddddd
Jay Curtis <jaysuzi5@gmail.com>
2025-05-09 08:13:41 -0400

    fix: adding call to member api

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ log_level = os.getenv("APP_LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=getattr(logging, log_level, logging.INFO))
 logging.getLogger('werkzeug').setLevel(getattr(logging, log_level, logging.INFO))
 app = Flask(__name__)
+redis_client = None
+INTERNAL_ERROR = "INTERNAL SERVER ERROR"
 
 
 def get_env_variable(var_name, default=None):
@@ -177,35 +179,55 @@ def get_latest_weather(n: int, transaction_id: str):
             conn.close()
     return response
 
-
-def load_redis(redis_client, user_id: str, first_name: str, last_name: str):
+def member_service(user_id):
+    fake = Faker()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
     user = {
         'userId': user_id,
         'firstName': first_name,
         'lastName': last_name
     }
-    redis_client.set(user_id, json.dumps(user))
     return user
 
 
-def check_redis(user_id: str):
-    redis_host = get_env_variable("REDIS_HOST")
-    redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=True)
-    user_data = redis_client.get(user_id)
-    if not user_data:
-        fake = Faker()
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        user_data = load_redis(redis_client, user_id, first_name, last_name)
+def load_redis(user_data: dict) -> None:
+    global redis_client
+    if not redis_client:
+        redis_host = get_env_variable("REDIS_HOST")
+        redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+    redis_client.set(user_data['userId'], json.dumps(user_data))
+
+
+def check_redis(user_id: str) -> dict:
+    global redis_client
+    if not redis_client:
+        redis_host = get_env_variable("REDIS_HOST")
+        redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+    return redis_client.get(user_id)
+
+def get_member(user_id: str) -> dict:
+    user_data = None
+    payload = {
+        'userId': user_id,
+    }
+    url = get_env_variable("MEMBER_MANAGEMENT_URL")
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        user_data = response.json()
     return user_data
 
 
 def authenticate_user(user_id: str):
-    # Random chance of 1 in 20 of not being found
+    # Random chance of 1 in 20 of not being authenticated
     if random.randint(1, 20) == 1:
         return None
-    else:
-        user_data = check_redis(user_id)
+
+    user_data = check_redis(user_id)
+    if not user_data:
+        user_data = get_member(user_id)
+        if user_data:
+            load_redis(user_data)
     return user_data
 
 
@@ -258,7 +280,7 @@ def login():
                 payload = {"error": "Error from authenticate service", "returnCode": response.status_code}
     except Exception as ex:
         return_code = 500
-        payload = {"error": "Internal Server Error", "details": str(ex)}
+        payload = {"error": INTERNAL_ERROR, "details": str(ex)}
     response_log(transaction_id, component, return_code, payload)
     return make_response(jsonify(payload), return_code)
 
@@ -285,6 +307,33 @@ def authenticate():
                 payload = user
     except Exception as ex:
         return_code = 500
-        payload = {"error": "Internal Server Error", "details": str(ex)}
+        payload = {"error": INTERNAL_ERROR, "details": str(ex)}
+    response_log(transaction_id, component, return_code, payload)
+    return make_response(jsonify(payload), return_code)
+
+
+@app.route("/member", methods=["POST"])
+def member():
+    return_code = 200
+    component = 'member'
+    transaction_id = None
+    try:
+        data = request.get_json()
+        user_id = data.get("userId", None)
+        payload = {
+            'userId': user_id,
+        }
+        transaction_id = request_log(component, payload)
+        if not user_id:
+            return_code = 400
+        else:
+            user = member_service(user_id)
+            if not user:
+                return_code = 401
+            else:
+                payload = user
+    except Exception as ex:
+        return_code = 500
+        payload = {"error": INTERNAL_ERROR, "details": str(ex)}
     response_log(transaction_id, component, return_code, payload)
     return make_response(jsonify(payload), return_code)
